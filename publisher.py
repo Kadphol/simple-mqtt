cff4e39c5f56b956ae5e3d1355011f32a85e4d40
Kadphol <kadphol.len@mail.kmutt.ac.th>
2020-02-23 21:37:16 +0700

    add publisher

---
+++ publisher.py
@@ -0,0 +1,27 @@
+from socket import *
+import sys
+
+SERV_PORT = 50000
+
+while True:
+    textout = sys.stdin.readline().strip()
+    publish = textout.split()
+
+    if(textout == 'exit'):
+        break
+    elif(len(publish) == 4):
+        if(publish[0] == 'publish'):
+            address = (publish[1], SERV_PORT)
+            try:
+                s = socket(AF_INET, SOCK_STREAM)
+                s.settimeout(5)
+                s.connect(address)
+                s.send(textout.encode('utf-8'))
+            except:
+                print('Invalid IP address')
+        else:
+            print('Invalid command!')
+    else:
+        print('Invalid argument')
+
+s.close()
