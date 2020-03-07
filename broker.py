3d46ff679b1efc67b7dd5aab3261deb8e9a90b2f
Kadphol <kadphol.len@mail.kmutt.ac.th>
2020-03-08 01:11:23 +0700

    renew broker

--- broker.py
+++ broker.py
@@ -2,72 +2,86 @@ from socket import *
 from threading import Thread
 import traceback
 import os,sys
+import shlex
 
+MAX_BUF = 2048
 SERV_PORT = 50000
 
 topics = {}
+ 
+def shutdown_socket():
+    for sockets in topics.values():
+        for s in sockets:
+            s.send('QUIT'.encode('utf-8'))
 
-def handle_client(s,addr):
+def delete_socket(conn_sock):
+    for sockets in topics.values():
+        for s in sockets:
+            if(s == conn_sock):
+                sockets.remove(conn_sock)
+
+def add_socket(conn_sock, topic):
+    if(topic in topics):
+        topics[topic].append(conn_sock)
+    else:
+        topics[topic] = conn_sock
+
+
+def handle_client(conn_sock, cli_sock_addr):
+  while True:
     try:
-        while True:
-            txtin = s.recv(1024)
-            value = txtin.decode('utf-8').split()
+        txtin = conn_sock.recv(2048)
+        txtin.decode('utf-8')
+        message = shlex.split(txtin)
 
-            if(value[0] == 'publish'):
-                print('Publish> %s %s from %s:%s' %(value[2],value[3],str(addr[0]),str(addr[1])))
-                for topic in topics:
-                    if(topic == value[2]):
-                        for subscriber in topics[value[2]]:
-                            subscriber.send(txt[3].encode('utf-8'))
-                s.close()
+        if(message[0] == 'subscribe'):
+            if(message[1] == 'quit'):
+                print('%s:%s disconneted' %(cli_sock_addr[0],cli_sock_addr[1]))
+                delete_socket(conn_sock)
                 break
-            elif(value[0] == 'subscribe'):
-                if(value[2] not in topics):
-                    subscriber = []
-                    subscriber.append(s)
-                    topics[value[2]] = subscriber
-                else:
-                    topics[value[2]].append(s)
-    except:
-        for topic in topics:
-            for subscriber in topics[topic]:
-                if(subscriber == s):
-                    topics[topic].remove(s)
-        print('%s:%s disconnected' %(str(addr[0]),str(addr[1])))
-    
-    s.close()
-    return
+            else:
+                print('%s:%s subscribe for topic %s' %(cli_sock_addr[0],cli_sock_addr[1],message[2]))
+                add_socket(conn_sock,message[2])
 
-def main(argv):
-    address = '127.0.0.1'
-    if(len(sys.argv) == 2):
-        address = str(sys.argv[1])
-    addr = (address, SERV_PORT)
-    s = socket(AF_INET, SOCK_STREAM)
-    s.bind(addr)
-    s.listen(5)
-    print ('Broker started at ' + address)
+        elif(message[0] == 'publish'):
+            if(message[2] in topics.keys()):
+                for s in topics[message[2]]:
+                    s.send(message[3].encode('utf-8'))
+            conn_sock.close()
+        break
+    except BlockingIOError:
+        pass
+
+
+def main():
+    serv_sock_addr = ('127.0.0.1', SERV_PORT)
+    welcome_sock.bind(serv_sock_addr)
+    welcome_sock.listen(5)
+    print ('Broker started at %s' %serv_sock_addr[0])
 
     while True:
-        sckt, addr = s.accept()
-        ip, port = str(addr[0]), str(addr[1]) 
-        print ('New client connected from ' + ip + ':' + port)
+        conn_sock, cli_sock_addr = welcome_sock.accept()
+        ip, port = str(cli_sock_addr[0]), str(cli_sock_addr[1]) 
+        print ('New client connected from ..' + ip + ':' + port)
     
         try:
-            Thread(target=handle_client, args=(sckt,addr)).start()
+            Thread(target=handle_client, args=(conn_sock, cli_sock_addr)).start()
         except:
-            print("Cannot start thread..")  
-            trackback.print_exc()
-    s.close()
+            print("Cannot start thread..")
+            trackback.print_exc()   
+
 
 if __name__ == '__main__':
     try:
-        main(sys.argv)
+        welcome_sock = socket(AF_INET, SOCK_STREAM)
+        main()
     except KeyboardInterrupt:
-        print ('Interrupted ..')
+        shutdown_socket()
+        print('')
+        print('Shut down socket')
+        welcome_sock.close()
+        print('Broker shutdown')
         try:
             sys.exit(0)
         except SystemExit:
             os._exit(0)
-
-
