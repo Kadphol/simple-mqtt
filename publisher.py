2139dfc9d7ba33ce0787f6c3c813cc10806dbdc4
Kadphol <kadphol.len@mail.kmutt.ac.th>
2020-03-08 11:24:42 +0700

    add comment

--- publisher.py
+++ publisher.py
@@ -7,23 +7,28 @@ SERV_PORT = 50000
 
 def main():
     while True:
-        textout = sys.stdin.readline().strip()
-        publish = shlex.split(textout)
+        try:
+            textout = sys.stdin.readline().strip() #read text from standard input
+            publish = shlex.split(textout) #split text with shell lexical split
 
-        if(textout == 'exit'):
-            break
-        elif(len(publish) == 4):
-            if(publish[0] == 'publish'):
-                address = (publish[1], SERV_PORT)
-                try:
-                    s.connect(address)
-                    s.send(textout.encode('utf-8'))
-                except:
-                    print('Invalid IP address')
+            if(textout == 'exit'): #exit
+                break
+            elif(len(publish) == 4): #check number of argument
+                if(publish[0] == 'publish'): #check command
+                    address = (publish[1], SERV_PORT) 
+                    try:
+                        s.connect(address) #connect socket
+                        s.send(textout.encode('utf-8')) #send textout
+                    except:
+                        print('Invalid IP address')
+                else:
+                    print('Invalid command!')
             else:
-                print('Invalid command!')
-        else:
-            print('Invalid argument')
+                print('Invalid argument')
+        except KeyboardInterrupt:
+            print('')
+            print('exit')
+            break
 
     s.close()
 
@@ -32,7 +37,6 @@ if __name__ == '__main__':
         s = socket(AF_INET, SOCK_STREAM)
         main()
     except KeyboardInterrupt:
-        print('')
         try:
             sys.exit(0)
         except SystemExit:
